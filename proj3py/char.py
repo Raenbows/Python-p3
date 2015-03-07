0bcba31def5159e930c4a25fb706d06111f96a72
Raenbows <rbyrkit@gmail.com>
2015-03-07 12:18:56 -0500

    upload to github. char.py and map.py

---
+++ proj3py/char.py
@@ -0,0 +1,35 @@
+#char.py
+
+class Character:
+
+    def __init__(self, hp, atk, dfc, name)
+        self.hp=hp
+        self.atk=atk
+        self.dfc=dfc
+
+class Player(Character):
+    def __init__(self, hp, atk, dfc, name)
+        Character.__init__(self, hp, atk, dfc, name)
+
+    def help():
+        pass
+    def health():
+        pass
+    def go():
+        pass
+    def attack(): 
+        pass
+    def quit():
+        pass
+
+class Hax(Player):
+    def __init__(self, hp, atk, dfc, name)
+        Player.__init__(self, 30, 8, 10, name)
+
+class Coder(Player):
+    def __init__(self, hp, atk, dfc, name)
+        Player.__init__(self, 30, 10, 8, name)
+
+class Robot(player):
+    def __init__(self, hp, atk, dfc, name)
+        Player.__init__(self, 15, 9, 7, "Robot")
---
+++ proj3py/map.py
@@ -0,0 +1,48 @@
+#map.py
+#Rachael Byrkit rmb11d
+from random import randint
+
+#0 : empty
+#1 : player
+#2 : enemy
+#3 : trap
+#4 : backpack
+
+class Map:
+    def __init__(self):
+        self.map=[[0 for x in range (5)] for x in range (5)]
+
+
+
+    def setMap(self):
+        #set player locationn
+        self.map[2][0]=1
+        #set trap
+        self.map[randint(0,4)][randint(0,4)] = 3
+        #set enemies
+        for x in range (0,4):
+            randx=randint(0,4)
+            randy=randint(0,4)
+            if self.map[randx][randy] is not 0:
+                break
+            else:
+                self.map[randx][randy]= 2
+        #set backpack
+        randx=randint(0,4)
+        randy=randint(0,4)
+        while self.map[randx][randy] is not 0:
+            randx=randint(0,4)
+            randy=randint(0,4)
+        else:
+            self.map[randx][randy]=4
+        
+                
+
+
+
+    #def nextMove:
+        
+
+
+    
+    
